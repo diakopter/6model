e35833ad8bb30178a89dd141fed8d843a0beb947
diakopter <diakopter@gmail.com>
2011-11-05 12:02:09 -0700

    implement missing P6mapping Ops

---
+++ lua/runtime/Runtime/Ops/P6mapping.lua
@@ -0,0 +1,28 @@
+function Ops.llmapping_get_at_key(TC, LLMapping, Key)
+    if (LLMapping.class == "P6mapping") then
+        local Storage = LLMapping.Storage;
+        local StrKey = Ops.unbox_str(TC, Key);
+        return Storage[StrKey];
+    else
+        error("Cannot use llmapping_get_at_key if representation is not P6mapping");
+    end
+end
+
+function Ops.llmapping_bind_at_key(TC, LLMapping, Key, Value)
+    if (LLMapping.class == "P6mapping") then
+        local Storage = LLMapping.Storage;
+        local StrKey = Ops.unbox_str(TC, Key);
+        Storage[StrKey] = Value;
+        return Value;
+    else
+        error("Cannot use llmapping_bind_at_key if representation is not P6mapping");
+    end
+end
+
+function Ops.llmapping_elems(TC, LLMapping)
+    if (LLMapping.class == "P6mapping") then
+        return Ops.box_int(TC, LLMapping.Storage.Count, TC.DefaultIntBoxType);
+    else
+        error("Cannot use llmapping_elems if representation is not P6mapping");
+    end
+end
